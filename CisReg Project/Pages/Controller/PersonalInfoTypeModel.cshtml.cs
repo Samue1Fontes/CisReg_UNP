e2f8279c5b8b70d0ba7abcb6880e3fff0ec596b9
dudublessed <idicasapple@icloud.com>
2024-10-04 09:55:57 -0300

    Correções...

---
+++ CisReg Project/Pages/Controller/PersonalInfoTypeModel.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CisReg_Project.Pages.Controller
+{
+    public class PersonalInfoModel : PageModel
+    {
+        [BindProperty]
+        public string SelectedRegistrationType { get; set; } = "";
+
+
+        public IActionResult OnPost()
+        {
+
+            return Page();
+        }
+    }
+}
