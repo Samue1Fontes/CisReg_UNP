cd210938f0b38cb6e3200bc96496139dbecdd3ce
dudublessed <idicasapple@icloud.com>
2024-10-04 17:23:43 -0300

    Projeto migrado para ASP.NET MVC.

---
+++ CisReg_Website/Models/PersonalInfoModel.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace CisReg_Website.Models
+{
+    [BindProperties]
+    public class PersonalInfoModel : PageModel
+    {
+        public string CompleteName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string CPF { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime BornDate { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("/register-professional-info");
+            }
+            return RedirectToPage("/registration-professional-info");
+        }
+    }
+}
---
+++ CisReg_Website/wwwroot/js/registration-type.js
@@ -0,0 +1,24 @@
+﻿
+// Views - Pages - RegistrationType.cshtml
+
+// Função que verifica se o usuário selecionou algum dos três tipos de cadastro.
+function validateForm() {
+    const selectedType = document.querySelector('input[name="SelectedRegistrationType"]:checked');
+
+    if (!selectedType) {
+        alert("Nenhum tipo de registro selecionado.");
+        return false;
+    }
+
+    const selectedValue = selectedType.value;
+
+    if (selectedType && selectedValue === 'ProfissionalDeSaude') {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+
