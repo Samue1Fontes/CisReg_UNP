176f016112cc8df813fb9e1790bc5a627c6ec10c
dudublessed <idicasapple@icloud.com>
2024-10-05 11:09:37 -0300

    Adicionado cabeçalhos de segurança.

--- CisReg_Website/Program.cs
+++ CisReg_Website/Program.cs
@@ -21,6 +21,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers["Content-Security-Policy"] = "default-src 'self';";
+
+    context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+
+    context.Response.Headers["X-XSS-Protection"] = "1; mode=block";
+
+    context.Response.Headers["X-Frame-Options"] = "DENY";
+
+    context.Response.Headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains";
+
+    await next();
+});
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
