8286a48bfe91a06994f2cbe5acabc057c6996994
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-10-31 21:09:03 -0300

    Alteração em Model

--- CisReg_Website/Models/UserModel.cs
+++ CisReg_Website/Models/UserModel.cs
@@ -12,4 +12,28 @@ public class UserModel : DataFoundation
 
   [BsonElement("email")]
   public string? Email { get; set; }
+
+    [BsonElement("adminPassword")]
+    public string? AdminPassword { get; set; }
+
+    [BsonElement("cep")]
+    public string? Cep { get; set; }
+
+    [BsonElement("position")]
+    public string? Position { get; set; }
+
+    [BsonElement("cpf")]
+    public string? Cpf { get; set; }
+
+    [BsonElement("userId")]
+    public string? UserId { get; set; }
+
+    [BsonElement("workHours")]
+    public string? WorkHours { get; set; }
+
+    [BsonElement("admissionDate")]
+    public DateTime AdmissionDate { get; set; }
+
+    [BsonElement("birthDate")]
+    public DateTime BirthDate { get; set; }
 }
