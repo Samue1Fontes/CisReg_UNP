4ac7e20f3288e85cc5fce0ffeadd89ea93462056
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-11-04 13:57:44 -0300

    alaterações na view delte, create, details, index. E no modelUser

--- CisReg_Website/Models/UserModel.cs
+++ CisReg_Website/Models/UserModel.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CisReg_Website.Models;
 
@@ -8,12 +9,19 @@ public class UserModel
   [BsonId]
   public ObjectId Id { get; set; }
 
-  [BsonElement("name")]
-  public string? Name { get; set; }
+    [BsonElement("first_name")]
+    [Display(Name = "Nome")]
+    public string? FirstName { get; set; }
 
-  [BsonElement("email")]
-  public string? Email { get; set; }
+    [BsonElement("last_name")]
+    [Display(Name = "Sobrenome")]
+    public string? LastName { get; set; }
+
+    [BsonElement("email")]
+    [Display(Name = "Email")]
+    public string? Email { get; set; }
 
   [BsonElement("password")]
-  public string? Password { get; set; }
+    [Display(Name = "Senha")]
+    public string? Password { get; set; }
 }
