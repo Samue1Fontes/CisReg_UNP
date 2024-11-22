a63f92b50caa96fbb88a7e63b7fe2fd2117712cc
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-11-21 22:19:46 -0300

    iniciando a colocar userSupController

---
+++ CisReg_Website/Controllers/SchudeleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CisReg_Website.Controllers
+{
+    public class schudeleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
